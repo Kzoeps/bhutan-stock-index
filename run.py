b10fa302e9a88936819dfd9056a8dfb48ed363ea
Kzoeps <kzoepa@gmail.com>
2020-11-29 22:58:58 +0600

    created the pyplots

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 import pandas as pd
-
+import matplotlib.pyplot as plt
 stockIndexes = pd.read_csv("./stockIndexClean.csv")
 twitterSentiments = pd.read_csv("./tweetSentiments.csv")
 
@@ -22,4 +22,32 @@ def create_matching_sets(stockIndex, twitterSentiments):
                matchingSet['Sentiment'].append(twitterSentiments.iloc[differentRow]['Sentiment Score'])
     return matchingSet
 
-print(create_matching_sets(stockIndexes, twitterSentiments))
+def scale_sentiment(sentiment):
+    """
+
+    :param sentiment: sentiment score
+    :return: scaledScore, sentiment score multiplied by 1000 so that it fits better on the graph
+    """
+    sentiment = float(sentiment)
+    scaledScore = sentiment*100
+    return scaledScore
+def create_plot(dataFrame):
+    """
+
+    :param dataFrame: dataFrame to extract the X and Y from.
+    :return: void, just draws a plot using matplotlib
+    """
+    X = dataFrame[0:]['Date']
+    Y1 = dataFrame[0:]['BSI']
+    Y2 = dataFrame[0:]['Sentiment']
+    fig, (ax1, ax2) = plt.subplots(1,2)
+    ax1.plot(X,Y1)
+    ax1.set_title('BSI')
+    ax2.plot(X, Y2)
+    ax2.set_title('Sentiment Scores')
+    fig.show()
+# stocksSentimentsDF = pd.DataFrame(create_matching_sets(stockIndexes, twitterSentiments))
+stocksSentimentsDF = pd.read_csv("./stocks&sentiments.csv")
+for eachRow in range(len(stocksSentimentsDF)):
+    stocksSentimentsDF.at[eachRow, 'Sentiment'] = scale_sentiment(stocksSentimentsDF.iloc[eachRow]['Sentiment'])
+create_plot(stocksSentimentsDF)
