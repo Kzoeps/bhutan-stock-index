96d8c5532dc63ee577391fe64be3d9eb6d8da3d5
Kzoeps <kzoepa@gmail.com>
2020-12-01 10:26:01 +0600

    ADD polynomial regression and plot

--- run.py
+++ run.py
@@ -1,8 +1,9 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-stockIndexes = pd.read_csv("./stockIndexClean.csv")
-twitterSentiments = pd.read_csv("./tweetSentiments.csv")
-
+import numpy as np
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.metrics import r2_score
 
 def create_matching_sets(stockIndex, twitterSentiments):
     """
@@ -31,21 +32,51 @@ def scale_sentiment(sentiment):
     sentiment = float(sentiment)
     scaledScore = sentiment*100
     return scaledScore
+def create_subplot(dataFrame):
+    """
+    Function to create subplots of sentiment and stock index with dates
+    :param dataFrame: dataFrame to extract the x and y from
+    :return: void, subplot with sentiment and stock index plot side by side
+    """
+    x = dataFrame[0:]['Date']
+    y1 = dataFrame[0:]['BSI']
+    y2 = dataFrame[0:]['Sentiment']
+    fig, (ax1, ax2) = plt.subplots(1,2)
+    ax1.scatter(x, y1)
+    ax1.set_title('Bhutan Stock Index')
+    ax2.scatter(x, y2)
+    ax2.set_title('Twitter Sentiment Scores')
+    fig.show()
 def create_plot(dataFrame):
     """
 
-    :param dataFrame: dataFrame to extract the X and Y from.
+    :param dataFrame: dataFrame to extract the x and y from.
     :return: void, just draws a plot using matplotlib
     """
-    X = dataFrame[0:]['Date']
-    Y1 = dataFrame[0:]['BSI']
-    Y2 = dataFrame[0:]['Sentiment']
-    fig, (ax1, ax2) = plt.subplots(1,2)
-    ax1.plot(X,Y1)
-    ax1.set_title('BSI')
-    ax2.plot(X, Y2)
-    ax2.set_title('Sentiment Scores')
-    fig.show()
+    y = np.array(dataFrame[0:]['BSI'])
+    x = np.array(dataFrame[0:]['Sentiment']).reshape((-1, 1))
+    x1 = np.array(dataFrame[0:]['Sentiment'])
+
+    mymodel = np.poly1d(np.polyfit(x1, y, 3))
+    myline = np.linspace(0,37,100)
+    rSq1 = r2_score(y, mymodel(x))
+    print(rSq1)
+
+    # change x to contain additional column with values = x^2
+    transformer = PolynomialFeatures(degree=2, include_bias=False)
+    x_ = transformer.fit_transform(x)
+    model = LinearRegression().fit(x_,y)
+    rSq = model.score(x_,y)
+    intercept = model.intercept_
+    slope = model.coef_
+    print('intercept :', intercept,'\nRSquared:',rSq,'\nSlope:', slope)
+    plt.scatter(x, y)
+    plt.plot(myline, mymodel(myline))
+    # plt.plot(x,slope[0]*x+intercept)
+    plt.show()
+
+stockIndexes = pd.read_csv("./stockIndexClean.csv")
+twitterSentiments = pd.read_csv("./tweetSentiments.csv")
 # stocksSentimentsDF = pd.DataFrame(create_matching_sets(stockIndexes, twitterSentiments))
 stocksSentimentsDF = pd.read_csv("./stocks&sentiments.csv")
 for eachRow in range(len(stocksSentimentsDF)):
